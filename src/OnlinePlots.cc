29ec73730233e4c0d381e67d54e082305ec1d3fa
Alexis Fagot <alexis.fagot@ugent.be>
2016-06-17 11:32:11 +0200

    [master]: added the ADC currents to the online plots

--- src/OnlinePlots.cc
+++ src/OnlinePlots.cc
@@ -192,14 +192,15 @@ void MakeRatePlots(Infrastructure Infra, string fName){
 void MakeCurrentPlots(Infrastructure Infra, string fName){
     //We need a vector of vectors of vectors... to keep
     //all the data in a dynamic way. This vector will
-    //contain 4 vector<vector<float>> : 1 for the HVeff,
-    //1 filled with 0 for the HVeffError, 1 for the Imon
-    //and 1 for the ImonErr.
+    //contain 6 vector<vector<float>> : 1 for the HVeff,
+    //1 filled with 0 for the HVeffError, 1 for the Imon,
+    //1 for the ImonErr, 1 for the ADC current and 1 for
+    //the ADC current error.
     //The vector filled with 0s will be important to use
     //the TGraphError objects
-    vector< vector<float> > Data[4];
+    vector< vector<float> > Data[6];
 
-    for(unsigned int i = 0; i < 4; i++)
+    for(unsigned int i = 0; i < 6; i++)
         Data[i].clear();
 
     //To be able to read the file in a dynamical way, we
@@ -236,6 +237,8 @@ void MakeCurrentPlots(Infrastructure Infra, string fName){
     Data[1].resize(TotGaps);
     Data[2].resize(TotGaps);
     Data[3].resize(TotGaps);
+    Data[4].resize(TotGaps);
+    Data[5].resize(TotGaps);
 
     //Open in reading mode the rate file
     ifstream CurrentFile(fName.c_str(), ios::in);
@@ -266,12 +269,16 @@ void MakeCurrentPlots(Infrastructure Infra, string fName){
                 for(unsigned int p = 0; p < TotGaps; p++){
                     float tmphv = 0;
                     float tmpimon = 0;
-                    float tmperr = 0;
-                    CurrentFile >> tmphv >> tmpimon >> tmperr;
+                    float tmpimonerr = 0;
+                    float tmpadc = 0;
+                    float tmpadcerr = 0;
+                    CurrentFile >> tmphv >> tmpimon >> tmpimonerr >> tmpadc >> tmpadcerr;
                     Data[0][p].push_back(tmphv);
                     Data[1][p].push_back(0.);
                     Data[2][p].push_back(tmpimon);
-                    Data[3][p].push_back(tmperr);
+                    Data[3][p].push_back(tmpimonerr);
+                    Data[4][p].push_back(tmpadc);
+                    Data[5][p].push_back(tmpadcerr);
                 }
             }
         }
@@ -291,10 +298,13 @@ void MakeCurrentPlots(Infrastructure Infra, string fName){
 
     for(unsigned int r = 0; r < TotRPCs; r++){
         TMultiGraph* ChamberCurrentsPlot = new TMultiGraph();
+        TMultiGraph* ChamberADCCurrentsPlot = new TMultiGraph();
 
         for(unsigned int p = 0; p < RPCGaps[r]; p++){
             if(Data[0][pi][0] != 0){
                 string Graphtitle = GapNames[pi];
+
+                //Imon plots
                 string Canvastitle = "Current-" + GapNames[pi];
 
                 TGraphErrors* PartitionCurrentPlot = new TGraphErrors(Data[0][pi].size(),&(Data[0][pi][0]),&(Data[2][pi][0]),&(Data[1][pi][0]),&(Data[3][pi][0]));
@@ -306,13 +316,33 @@ void MakeCurrentPlots(Infrastructure Infra, string fName){
                 PartitionCurrentPlot->SetMarkerColor(p+2);
                 PartitionCurrentPlot->SetMarkerStyle(p+20);
 
-                TCanvas* cPart = new TCanvas(Canvastitle.c_str());
-                cPart->cd(0);
-                ChamberCurrentsPlot->Draw("ap");
-                cPart->BuildLegend();
-                cPart->Write();
+                TCanvas* cImon = new TCanvas(Canvastitle.c_str());
+                cImon->cd(0);
+                PartitionCurrentPlot->Draw("ap");
+                cImon->BuildLegend();
+                cImon->Write();
 
                 ChamberCurrentsPlot->Add(PartitionCurrentPlot);
+
+                //ADC current plots
+                Canvastitle = "ADC-" + GapNames[pi];
+
+                TGraphErrors* PartitionADCCurrentPlot = new TGraphErrors(Data[0][pi].size(),&(Data[0][pi][0]),&(Data[4][pi][0]),&(Data[1][pi][0]),&(Data[5][pi][0]));
+                PartitionADCCurrentPlot->SetTitle(Graphtitle.c_str());
+                PartitionADCCurrentPlot->GetXaxis()->SetTitle("HV_{eff}(V)");
+                PartitionADCCurrentPlot->GetYaxis()->SetTitle("Mean ADC current (#muA/cm^{2})");
+                PartitionADCCurrentPlot->GetXaxis()->SetRangeUser(Data[0][pi].front(),Data[0][pi].back());
+                PartitionADCCurrentPlot->SetLineColor(p+2);
+                PartitionADCCurrentPlot->SetMarkerColor(p+2);
+                PartitionADCCurrentPlot->SetMarkerStyle(p+20);
+
+                TCanvas* cAdc = new TCanvas(Canvastitle.c_str());
+                cAdc->cd(0);
+                PartitionADCCurrentPlot->Draw("ap");
+                cAdc->BuildLegend();
+                cAdc->Write();
+
+                ChamberADCCurrentsPlot->Add(PartitionADCCurrentPlot);
             } else {
                 string Graphtitle = GapNames[pi];
 
@@ -322,31 +352,56 @@ void MakeCurrentPlots(Infrastructure Infra, string fName){
                 PartitionCurrentPlot->GetYaxis()->SetTitle("Mean current (#muA/cm^{2})");
 
                 ChamberCurrentsPlot->Add(PartitionCurrentPlot);
+
+                TGraphErrors* PartitionADCCurrentPlot = new TGraphErrors();
+                PartitionADCCurrentPlot->SetTitle(Graphtitle.c_str());
+                PartitionADCCurrentPlot->GetXaxis()->SetTitle("HV_{eff}(V)");
+                PartitionADCCurrentPlot->GetYaxis()->SetTitle("Mean ADC current (#muA/cm^{2})");
+
+                ChamberADCCurrentsPlot->Add(PartitionADCCurrentPlot);
             }
             pi++;
         }
-
-        string MultigraphTitle = "Mean monitored current evolution in " + RPCNames[r];
-        string MultiCanvasTitle = "Current-" + RPCNames[r];
 
         string DQMFolder = fName.substr(0,fName.find_last_of("/")) + "/Online/";
         string mkdirDQMFolder = "mkdir -p " + DQMFolder;
         system(mkdirDQMFolder.c_str());
 
-        TCanvas* cMulti = new TCanvas(MultiCanvasTitle.c_str());
-        cMulti->cd(0);
+        string MultigraphTitle = "Mean monitored current evolution in " + RPCNames[r];
+        string MultiCanvasTitle = "Current-" + RPCNames[r];
+
+        TCanvas* cMultiImon = new TCanvas(MultiCanvasTitle.c_str());
+        cMultiImon->cd(0);
         ChamberCurrentsPlot->Draw("ap");
         ChamberCurrentsPlot->SetMinimum(0.);
         ChamberCurrentsPlot->SetTitle(MultigraphTitle.c_str());
         ChamberCurrentsPlot->GetXaxis()->SetTitle("HV_{eff}(V)");
         ChamberCurrentsPlot->GetYaxis()->SetTitle("Mean current (#muA/cm^{2})");
-        cMulti->BuildLegend(0.15,0.67,0.45,0.88);
-        cMulti->Update();
-        string PDF = DQMFolder + cMulti->GetName() + ".pdf";
-        string PNG = DQMFolder + cMulti->GetName() + ".png";
-        cMulti->SaveAs(PDF.c_str());
-        cMulti->SaveAs(PNG.c_str());
-        cMulti->Write();
+        cMultiImon->BuildLegend(0.15,0.67,0.45,0.88);
+        cMultiImon->Update();
+        string PDF = DQMFolder + cMultiImon->GetName() + ".pdf";
+        string PNG = DQMFolder + cMultiImon->GetName() + ".png";
+        cMultiImon->SaveAs(PDF.c_str());
+        cMultiImon->SaveAs(PNG.c_str());
+        cMultiImon->Write();
+
+        MultigraphTitle = "Mean ADC current evolution in " + RPCNames[r];
+        MultiCanvasTitle = "ADC-" + RPCNames[r];
+
+        TCanvas* cMultiADC = new TCanvas(MultiCanvasTitle.c_str());
+        cMultiADC->cd(0);
+        ChamberADCCurrentsPlot->Draw("ap");
+        ChamberADCCurrentsPlot->SetMinimum(0.);
+        ChamberADCCurrentsPlot->SetTitle(MultigraphTitle.c_str());
+        ChamberADCCurrentsPlot->GetXaxis()->SetTitle("HV_{eff}(V)");
+        ChamberADCCurrentsPlot->GetYaxis()->SetTitle("Mean ADC current (#muA/cm^{2})");
+        cMultiADC->BuildLegend(0.15,0.67,0.45,0.88);
+        cMultiADC->Update();
+        PDF = DQMFolder + cMultiADC->GetName() + ".pdf";
+        PNG = DQMFolder + cMultiADC->GetName() + ".png";
+        cMultiADC->SaveAs(PDF.c_str());
+        cMultiADC->SaveAs(PNG.c_str());
+        cMultiADC->Write();
     }
 
     fROOT.Close();
